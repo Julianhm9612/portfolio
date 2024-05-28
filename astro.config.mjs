c90954c9be6bd963ca5520aa0195e117d0eed826
Julian Henao Marin <julianhm9612@gmail.com>
2024-05-28 17:19:59 -0500

    Fix base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import astroI18next from 'astro-i18next';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), astroI18next()],
+    site: "https://julianhm9612.github.io",
+    base: "/project",
+    integrations: [tailwind(), astroI18next()]
 });
